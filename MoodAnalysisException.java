fa574b39fa8ceaf9452163d84b7fae88fde056cb
Rajudv19 <rajudv1997@gmail.com>
2022-05-18 09:46:04 +0530

    Added code for MoodAnalysisException

---
+++ MoodAnalysisException.java
@@ -0,0 +1,10 @@
+package com.BridgeLabs.day21;
+
+public class MoodAnalysisException extends Exception {
+
+	String message;
+
+	public MoodAnalysisException(String message) {
+		super(message);
+	}
+}
